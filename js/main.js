499570a188c7026c173a642a5f4b5b4652d51520
AIZENCODE <migelo5511@gmail.com>
2023-05-11 10:35:57 -0500

    cambios

--- js/main.js
+++ js/main.js
@@ -1,3 +1,27 @@
+
+const usuario = 
+  {
+    name:'roxana sanchez peña',
+    email:'diego.saria@gmail.com',
+    rol:'administrador'
+  };
+const nombreCompleto = String(usuario.name);
+
+
+const palabras = nombreCompleto.split(" ");
+let iniciales = "";
+for (let i = 0; i < 2; i++) {
+  iniciales += palabras[i].charAt(0).toUpperCase();
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+
+  
+perfil.textContent = iniciales;
+
+})
+const perfil = document.getElementById('perfil');
 const menu = document.getElementById('menu');
 const menuclose = document.getElementById('menu_close');
 const nav = document.getElementById('nav');
